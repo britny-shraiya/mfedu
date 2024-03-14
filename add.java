253da1937bf75d13d0dcc66f936ba884f213c266
britny-shraiya <163390773+britny-shraiya@users.noreply.github.com>
2024-03-14 21:08:11 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,39 @@
+public class add {
+    abstract class Addition {
+        abstract void add();
+    
+        public void sub() {
+            System.out.println("Subtraction");
+        }
+    }
+    
+    abstract class ImplementedMethod extends Addition {
+        abstract void division();
+    
+        public void multiply() {
+            System.out.println("Multiplication");
+        }
+    }
+    
+    public class AbstractExample {
+        public static void main(String[] args) {
+            ImplementedMethod obj = new ImplementedMethod() {
+                @Override
+                void add() {
+                    System.out.println("Addition");
+                }
+    
+                @Override
+                void division() {
+                    System.out.println("Division");
+                }
+            };
+    
+            obj.add();
+            obj.sub();
+            obj.multiply();
+            obj.division();
+        }
+    }
+    
+}
